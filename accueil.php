0b03147d67644b4ffe1975c0a93a0b7054b840ee
Alimifils <46746806+Alimifils@users.noreply.github.com>
2019-10-21 16:41:37 +0200

    Amélioration de la page d'accueil.

--- accueil.php
+++ accueil.php
@@ -1,5 +1,11 @@
-<?php
-  $titre_pojet="Gestion des tâches et des évements";
-
-   echo '<h1>'.$titre_projet.'</h1>';	
-?>
+<html>
+  <head>
+     <title>Gestion des tâches et évéments</title>
+   <body>
+      <?php
+       $titre_pojet="Gestion des tâches et des évements";
+        echo '<h1>'.$titre_projet.'</h1>';	
+      ?>
+     <p>Ce projet est encours de développpement.</p>
+    </body>
+  </html>
